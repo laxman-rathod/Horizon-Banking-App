26ba83365613d443539d610c2f6ba5d7d1a4dd69
Laxman Rathod <laxmanrathod.dev@gmail.com>
2024-08-08 01:24:59 +0530

    Implement recent transactions

--- components/RecentTransacions.tsx
+++ components/RecentTransacions.tsx
@@ -1,5 +1,67 @@
-const RecentTransacions = () => {
-  return <div>RecentTransacions</div>;
+import Link from "next/link";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { BankTabItem } from "./BankTabItem";
+import BankInfo from "./BankInfo";
+import TransactionsTable from "./TransactionsTable";
+
+const RecentTransacions = ({
+  page = 1,
+  accounts,
+  transactions = [],
+  appwriteItemId,
+}: RecentTransactionsProps) => {
+  const rowsPerPage = 10;
+  const totalPages = Math.ceil(transactions.length / rowsPerPage);
+
+  const indexOfLastTransaction = page * rowsPerPage;
+  const indexOfFirstTransaction = indexOfLastTransaction - rowsPerPage;
+
+  const currentTransactions = transactions.slice(
+    indexOfFirstTransaction,
+    indexOfLastTransaction
+  );
+
+  return (
+    <section className="recent-transactions">
+      <header className="flex items-center justify-between">
+        <h2 className="recent-transactions-label">Recent Transactions</h2>
+        <Link
+          href={`/transaction-history/?id=${appwriteItemId}`}
+          className="view-all-btn"
+        >
+          View all
+        </Link>
+      </header>
+
+      <Tabs defaultValue={appwriteItemId} className="w-full">
+        <TabsList className="recent-transactions-tablist">
+          {accounts.map((account: Account) => (
+            <TabsTrigger key={account.id} value={account.appwriteItemId}>
+              <BankTabItem
+                key={account.id}
+                account={account}
+                appwriteItemId={appwriteItemId}
+              />
+            </TabsTrigger>
+          ))}
+        </TabsList>
+        {accounts.map((account: Account) => (
+          <TabsContent
+            key={account.id}
+            value={account.appwriteItemId}
+            className="space-y-4"
+          >
+            <BankInfo
+              type="full"
+              account={account}
+              appwriteItemId={appwriteItemId}
+            />
+            <TransactionsTable transactions={transactions} />
+          </TabsContent>
+        ))}
+      </Tabs>
+    </section>
+  );
 };
 
 export default RecentTransacions;
